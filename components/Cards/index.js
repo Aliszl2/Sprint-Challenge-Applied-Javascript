fb2ab7e000d3a162aa951844817cdff0f5aa3edc
Alison Law <alisparklaw@gmail.com>
2020-01-17 10:57:51 +0000

    cards populating

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,55 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const authorArray = [];
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then((response) => {
+        console.log(response);
+        for (let i = 0; i < response.data.articles.javascript.length; i++) {
+            authorArray.push(response.data.articles.javascript[i])
+        }
+        authorArray.forEach(args => {
+            newCard.append(createCard(args.headline, args.authorPhoto, args.authorName));   
+        })
+        console.log(authorArray.length);
+        console.log(authorArray);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+console.log(authorArray);
+
+
+function createCard(headline, authorPhoto, authorName) {
+
+    const card = document.createElement('div');
+    const headLine = document.createElement('div');
+    const authorDiv = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const authorImg = document.createElement('img');
+    const name = document.createElement('span');
+
+  
+    card.append(headLine);
+    card.append(authorDiv);
+    authorDiv.append(imgContainer);
+    imgContainer.append(authorImg);
+    authorDiv.append(name);
+
+
+    card.classList.add('card');
+    headLine.classList.add('headline');
+    authorDiv.classList.add('author');
+    imgContainer.classList.add('img-container')
+
+
+    headLine.textContent = headline;
+    authorImg.setAttribute('src', authorPhoto);
+    authorImg.textContent = authorPhoto;
+    name.textContent = authorName;
+
+    return card;
+}
+
+const newCard = document.querySelector('.cards-container');
+console.log(newCard);
