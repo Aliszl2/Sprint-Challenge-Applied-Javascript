f82d6871dd78778ec4634ca14a88709fa93becd0
Alison Law <alisparklaw@gmail.com>
2020-01-17 10:11:32 +0000

    header and axios request tabs

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,21 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function header(date, title, temp) {
+    const pageHeader = document.createElement("div");
+    const todaysDate = document.createElement("span");
+    const LambdaTimesTitle = document.createElement("h1");
+    const temperature = document.createElement("span");
+    pageHeader.append(todaysDate);
+    pageHeader.append(LambdaTimesTitle);
+    pageHeader.append(temperature);
+    pageHeader.classList.add("header");
+    todaysDate.classList.add("date");
+    temperature.classList.add("temp");
+    todaysDate.textContent = date;
+    LambdaTimesTitle.textContent = title;
+    temperature.textContent = temp;
+    return pageHeader;
+    }
+    const lambdaTimesHeader = document.querySelector(".header-container");
+    lambdaTimesHeader.append(header(`JAN 17, 2020`, `Lambda Times`, `98 deg`));
